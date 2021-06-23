f6b0c51b0807eb464486ecbaea39540e6d3f51fe
Stjepan Posavec <stjepan.posavec@gmail.com>
2021-06-23 13:53:22 +0200

    refresh 404 fixed on hosting and ready for styling chatroom

--- src/views/ChatRoom.tsx
+++ src/views/ChatRoom.tsx
@@ -4,7 +4,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import { Messages } from '../components/chat/Messages';
 import { rootReducerState } from '../models';
 import { encryptMessage } from '../modules/crypt/cryptMessage';
-import { firestore } from '../modules/firebase/firebaseProvider';
+import { auth, firestore } from '../modules/firebase/firebaseProvider';
 
 interface ChatRoomProps {
   key: string;
@@ -17,6 +17,7 @@ export const ChatRoom: React.FC = () => {
     (state: rootReducerState) => state.user.data?.username,
   );
   const history = useHistory();
+  const currentLoggedUser = auth.currentUser ? true : false;
 
   const onChangeMessage = (event: ChangeEvent<HTMLInputElement>) => {
     setMessage(event.currentTarget.value);
@@ -60,11 +61,17 @@ export const ChatRoom: React.FC = () => {
 
   return (
     <div>
-      <p>key: {key}</p>
-      <Messages roomKey={key} />
-      <input type="text" value={message} onChange={onChangeMessage} />
-      <button onClick={onMessageSend}>Send</button>
-      <button onClick={onClickDeleteRoom}>Delete Room</button>
+      {currentLoggedUser ? (
+        <>
+          <p>key: {key}</p>
+          <Messages roomKey={key} />
+          <input type="text" value={message} onChange={onChangeMessage} />
+          <button onClick={onMessageSend}>Send</button>
+          <button onClick={onClickDeleteRoom}>Delete Room</button>
+        </>
+      ) : (
+        <h1 className="heading">You need to be logged in.</h1>
+      )}
     </div>
   );
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { CreateRoom } from '../components/home/CreateRoom';
 import { JoinRoom } from '../components/home/JoinRoom';
+import { auth } from '../modules/firebase/firebaseProvider';
 
 export const Home: React.FC = () => {
+  const currentUser = auth.currentUser ? true : false;
   return (
-    <div>
-      <CreateRoom />
-      <JoinRoom />
+    <div className="auth-container">
+      {currentUser ? (
+        <>
+          <CreateRoom />
+          <JoinRoom />
+        </>
+      ) : (
+        <h1 className="heading">You need to be logged in.</h1>
+      )}
     </div>
   );
 };
